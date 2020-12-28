4914ec4700bc7a46275f9a741faf1d4dce50034d
Victor <victor.svilar@gmail.com>
2020-12-28 18:52:31 -0300

    aprendendo sobre os outros layouts

--- src/br/com/landtecengenharia/sistemaInterno/layouts/formularios/FormularioFlowLayout.java
+++ src/br/com/landtecengenharia/sistemaInterno/layouts/formularios/FormularioFlowLayout.java
@@ -1,31 +1,33 @@
 package br.com.landtecengenharia.sistemaInterno.layouts.formularios;
-import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class FormularioBorderLayout {
+public class FormularioFlowLayout {
 	JFrame form = new JFrame();
 	JButton botao = new JButton("Teste");
 	JButton botao2 = new JButton("Teste");
 	JButton botao3 = new JButton("Teste");
-	public FormularioBorderLayout() {
-		//layout padra, não é necessario
-		form.setLayout(new BorderLayout());
+	public FormularioFlowLayout() {
+		// os componentes vao ocupando a linhas em quanto tiver espaço
+		// passando a constante eles irao alinhados 
+		form.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		form.setTitle("Título da Janela");
 		form.setSize(700,600);
 		form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		form.setLocationRelativeTo(null);
 		form.setVisible(true);
-		
+	
 		//incluindo objetos com o borer layout
-		form.add(BorderLayout.NORTH,botao);
-		form.add(BorderLayout.EAST,botao2);
-		form.add(BorderLayout.CENTER,botao3);
+		form.add(botao);
+		form.add(botao2);
+		form.add(botao3);
+		
 	}
 	
 	public static void main(String[] args) {
-		new FormularioBorderLayout();
+		new FormularioFlowLayout();
 	}
 	
 
