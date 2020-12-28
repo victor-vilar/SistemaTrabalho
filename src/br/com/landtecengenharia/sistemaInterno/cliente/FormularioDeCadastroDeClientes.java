40d3bf318f28d9c443256fd19318ca456f5eca88
Victor <victor.svilar@gmail.com>
2020-12-28 14:26:42 -0300

    colocando o cpf e o cpnj no formulario

--- src/br/com/landtecengenharia/sistemaInterno/cliente/FormularioDeCadastroDeClientes.java
+++ src/br/com/landtecengenharia/sistemaInterno/cliente/FormularioDeCadastroDeClientes.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-
+import javax.swing.JOptionPane;
 public class FormularioDeCadastroDeClientes implements ActionListener {
 	
 	JFrame frame;
@@ -15,40 +15,71 @@ public class FormularioDeCadastroDeClientes implements ActionListener {
 	JButton botaoFechar;
 	Label l1;
 	TextField nomeDoCliente;
+	Label cnpj;
+	TextField cnpjDoCliente;
+	Label cpf;
+	TextField cpfDoCliente;
 	
 	public FormularioDeCadastroDeClientes(){
 		
+		//objetos
+		//labels
+		l1 = new Label("Nome:");
+		cnpj = new Label("CNPJ:");
+		cpf = new Label("CPF:");
+		//botoes
 		frame = new JFrame();
 		botaoSalvar = new JButton("Salvar");
 		botaoFechar = new JButton("Fechar");
-		l1 = new Label("Nome:");
+		//textfield
 		nomeDoCliente = new TextField("",255);
+		cnpjDoCliente = new TextField("",14);
+		cpfDoCliente = new TextField("", 11);
 		
-		botaoSalvar.addActionListener(this);
-		botaoFechar.addActionListener(this);
-		frame.setTitle("Cadastro de Clientes");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300,300);
-		frame.setVisible(true);	
+		//----------
+		//adicionando no formulario
 		frame.add(botaoSalvar);
-		frame.add(botaoFechar);
-		frame.setLayout(null);
-		frame.setResizable(false);
-		botaoSalvar.setBounds(5,225,100,30);					
-		botaoFechar.setBounds(110,225,100,30);
+		frame.add(botaoFechar);		
 		frame.add(nomeDoCliente);
 		frame.add(l1);
+		frame.add(cnpj);
+		frame.add(cnpjDoCliente);
+		frame.add(cpf);
+		frame.add(cpfDoCliente);
+		//------------------------------------
+		//adicionando os listeneres nos botoes
+		botaoSalvar.addActionListener(this);
+		botaoFechar.addActionListener(this);
+		//------------------------------------
+		//informaçoes da janela
+		frame.setTitle("Cadastro de Clientes");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
+		frame.setVisible(true);	
+		frame.setLayout(null);
+		frame.setResizable(false);
+		//------------------------------------
+		//informações dos objetos
+		//botoes
+		botaoSalvar.setBounds(5,225,100,30);					
+		botaoFechar.setBounds(110,225,100,30);
+		//labels
+		cpf.setBounds(5,60, 200, 20);
+		cnpj.setBounds(200,60, 200, 20);
 		l1.setBounds(5,5, 100, 30);
-		nomeDoCliente.setBounds(5,30, 250, 20);
-		//nomeDoCliente.
-		
-			
+		//textfield
+		nomeDoCliente.setBounds(5,30, 475, 20);
+		cnpjDoCliente.setBounds(200,80, 150, 20);
+		cpfDoCliente.setBounds(5,80, 150, 20);
+		//------------------------------------
+		//nomeDoCliente.;
 	}
-
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getSource() == botaoSalvar) {
+			JOptionPane.showMessageDialog(null,"Cliente " + nomeDoCliente.getText() + " salvo com sucesso !");
+		}
 		
 	}
 	
