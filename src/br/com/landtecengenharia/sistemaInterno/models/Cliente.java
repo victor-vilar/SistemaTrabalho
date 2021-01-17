a588b6db42b61ff80172c3d0670d05bdf6dc14dc
Victor <victor.svilar@gmail.com>
2021-01-17 19:46:41 -0300

    adicionando lista de endereços nos clientes

--- src/br/com/landtecengenharia/sistemaInterno/models/Cliente.java
+++ src/br/com/landtecengenharia/sistemaInterno/models/Cliente.java
@@ -1,10 +1,13 @@
 package br.com.landtecengenharia.sistemaInterno.models;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente implements Serializable{
 
 	private String nome;
 	private String cpfcnpj;
+	private List<Address> enderecos = new ArrayList<>();
 	
 
 	public Cliente(String nome, String cpfcnpj) {
@@ -16,12 +19,12 @@ public class Cliente implements Serializable{
 		return nome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public String getCpfCnpj() {
 		return cpfcnpj;
+	}
+	
+	public void addEndereco(Address endereco) {
+		this.enderecos.add(endereco);
 	}
 		
 }
